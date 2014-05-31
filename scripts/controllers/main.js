27560c13ac2f4f53f92fe5b4b469b33251386db4
Moritz Schäfer <mail@moritzs.de>
2014-05-31 02:41:17 +0200

    working start site

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -2,9 +2,18 @@
 
 angular.module('newOliverApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+      $scope.mainSlides = [
+          "01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"];
+      $scope.afterSlideshow = function($slider) {
+          $('.current_pos').text($slider.currentSlide+1);
+      }
+      $scope.startSlideshow = function($slider) {
+        $('.total').text($slider.count);
+      }
+      $scope.nextSlideshow = function() {
+          $(".flexslider").flexslider('next');
+      }
+      $scope.prevSlideshow = function() {
+          $(".flexslider").flexslider('prev');
+      }
   });
