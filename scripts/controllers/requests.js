235f8b6ed073f1740f10aa677865ed6cd3bad927
Moritz Schäfer <mail@moritzs.de>
2014-06-11 20:27:39 +0200

    fix update list issue

--- scripts/controllers/requests.js
+++ scripts/controllers/requests.js
@@ -4,6 +4,9 @@ angular.module('newOliverApp')
 .controller('RequestsCtrl', ["$scope", "requests", "$http", function ($scope, requests, $http) {
         $scope.showOverlay = false;
         $scope.$watch(function() {return requests.requests;}, function(newVal) {
+            updateRequests();
+        });
+        var updateRequests = function() {
             $scope.requests = requests.requests.map(function (element, index) {
                 return {
                     title: requests.selection[element.id].title,
@@ -12,20 +15,21 @@ angular.module('newOliverApp')
                     price: requests.requestOptions[element.requestOption].price
                 };
             });
-        });
+        }
         $scope.deleteItem = function(id) {
             requests.deleteRequest(id);
+            updateRequests();
         }
         $scope.sendRequest = function() {
             // prepare data
-            var requestsText = requests.requests.map(function (element, index) {
+            var requestsTexts = requests.requests.map(function (element, index) {
                 return
                 requests.selection[element.id].title + "\n" +
                     requests.requestOptions[element.requestOption].description + "\n" +
                     requests.requestOptions[element.requestOption].price + "\n";
             });
 
-            var data = {email: $scope.email, name: $scope.name, message: $scope.message, requests: requestsText.join("\n")};
+            var data = {email: $scope.email, name: $scope.name, message: $scope.message, requests: requestsTexts.join("\n")};
             $scope.showOverlay = true;
             // send email
             $http({method: 'POST', url: '/backend/mailer.php', data: data});
